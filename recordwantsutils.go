9d9196a9794c829a5af43d4d5f337d54baad3930
Simon Tucker <brotherlogic@gmail.com>
2018-02-04 10:48:37 -0800

    Added logic loop. This closes #4

--- recordwantsutils.go
+++ recordwantsutils.go
@@ -3,3 +3,11 @@ package main
 func forTest() {
 
 }
+
+func wantsRun() {
+	//Get the records
+
+	//Compute the two month rolling average of spendFlags
+
+	// If spend exceeds allowance, collapse wants (set active: false)
+}
