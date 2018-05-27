c0d2e5d86a1efcd8776a7b8998b7dae7216a1f7b
Simon Tucker <brotherlogic@gmail.com>
2018-05-27 12:21:48 -0700

    Added alerting on poor staging. This closes #16

--- recordwantsutils.go
+++ recordwantsutils.go
@@ -1,5 +1,11 @@
 package main
 
-func forTest() {
+import "context"
 
+func (s *Server) alertNoStaging(ctx context.Context) {
+	for _, want := range s.config.Wants {
+		if !want.Staged {
+			s.alerter.alert(ctx, want)
+		}
+	}
 }
--- recordwantsutils_test.go
+++ recordwantsutils_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/recordwants/proto"
+)
+
+type testAlerter struct {
+	count int
+}
+
+func (t *testAlerter) alert(ctx context.Context, want *pb.MasterWant) {
+	t.count++
+}
 
 func TestMainTest(t *testing.T) {
-	forTest()
+	s := InitTestServer()
+	s.config.Wants = append(s.config.Wants, &pb.MasterWant{Staged: false})
+	ta := &testAlerter{}
+	s.alerter = ta
+	s.alertNoStaging(context.Background())
+
+	if ta.count != 1 {
+		t.Errorf("Not enough alerts!")
+	}
 }
