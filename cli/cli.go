f1c298265e6f0bfa274d02af416205242930776f
Simon Tucker <brotherlogic@gmail.com>
2018-07-15 12:29:39 -0700

    Added CLI

--- cli/cli.go
+++ cli/cli.go
@@ -53,6 +53,12 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error on GET: %v", err)
 		}
+	case "unwant":
+		iv, _ := strconv.Atoi(os.Args[2])
+		_, err := client.Update(ctx, &pb.UpdateRequest{Want: &pbgd.Release{Id: int32(iv)}})
+		if err != nil {
+			log.Fatalf("Error on GET: %v", err)
+		}
 
 	}
 
