376db1470db731d768a4c57fe82cc72d3b89617e
Simon Tucker <brotherlogic@gmail.com>
2020-08-20 23:15:58 -0700

    Reduce speed now

--- recordwantsapi.go
+++ recordwantsapi.go
@@ -75,11 +75,11 @@ func (s *Server) Update(ctx context.Context, req *pb.UpdateRequest) (*pb.UpdateR
 
 //ClientUpdate on an updated record
 func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
+	return &rcpb.ClientUpdateResponse{}, nil
 
 	err := s.updateWants(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return &rcpb.ClientUpdateResponse{}, s.updateWantState(ctx)
 	return &rcpb.ClientUpdateResponse{}, s.updateWantState(ctx)
 }
